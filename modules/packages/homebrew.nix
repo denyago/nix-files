7c9cc8f508f5d69fc21968ca37a622184558619d
Denys Yahofarov <denyago@gmail.com>
2026-06-14 17:10:13 +0200

    fix nix brew cleanup deprecation warning

--- modules/packages/homebrew.nix
+++ modules/packages/homebrew.nix
@@ -17,7 +17,7 @@ in
 
       homebrew = {
         enable = true;
-        onActivation.cleanup = flakeConfig.homebrewCleanup;
+        onActivation.cleanup = "none";
         onActivation.extraEnv.HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS = "1";
         onActivation.extraEnv.HOMEBREW_NO_ENV_HINTS = "1";
 
@@ -58,5 +58,47 @@ in
         eval "$(${config.homebrew.prefix}/bin/brew shellenv)"
         export FPATH="$(brew --prefix)/share/zsh/site-functions:$FPATH"
       '';
+
+      system.activationScripts.postActivation.text = lib.mkAfter (
+        if flakeConfig.homebrewCleanup == "none" then
+          ""
+        else if flakeConfig.homebrewCleanup == "zap" then
+          ''
+            # Homebrew cleanup without deprecated brew bundle --cleanup
+            if [ -f "${config.homebrew.prefix}/bin/brew" ]; then
+              PATH="${config.homebrew.prefix}/bin:$PATH" \
+              sudo \
+                --preserve-env=PATH \
+                --user=${flakeConfig.username} \
+                --set-home \
+                env \
+                HOMEBREW_NO_AUTO_UPDATE=1 \
+                HOMEBREW_NO_ENV_HINTS=1 \
+                HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS=1 \
+                ${config.homebrew.prefix}/bin/brew bundle cleanup \
+                  --force \
+                  --zap \
+                  --file='${config.homebrew.brewfile}'
+            fi
+          ''
+        else
+          ''
+            # Homebrew cleanup without deprecated brew bundle --cleanup
+            if [ -f "${config.homebrew.prefix}/bin/brew" ]; then
+              PATH="${config.homebrew.prefix}/bin:$PATH" \
+              sudo \
+                --preserve-env=PATH \
+                --user=${flakeConfig.username} \
+                --set-home \
+                env \
+                HOMEBREW_NO_AUTO_UPDATE=1 \
+                HOMEBREW_NO_ENV_HINTS=1 \
+                HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS=1 \
+                ${config.homebrew.prefix}/bin/brew bundle cleanup \
+                  --force \
+                  --file='${config.homebrew.brewfile}'
+            fi
+          ''
+      );
     };
 }
