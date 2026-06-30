af58ac5ec587b9cb9f9db5a96ff99df54afa2975
Denys Yahofarov <denyago@gmail.com>
2026-06-30 20:29:46 +0200

    Update base

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -4,9 +4,17 @@
     programs.git = {
       enable = true;
       signing.format = null;
-      settings.user = {
-        name = config.fullName;
-        email = config.email;
+      settings = {
+        user = {
+          name = config.fullName;
+          email = config.email;
+        };
+        filter.lfs = {
+          clean = "git-lfs clean -- %f";
+          smudge = "git-lfs smudge -- %f";
+          process = "git-lfs filter-process";
+          required = true;
+        };
       };
     };
   };
